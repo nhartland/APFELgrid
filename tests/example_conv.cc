bf145ae5778f3e82cfe087d26adda8c5f888f328
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2016-04-19 18:05:57 +0100

    notation for docco

--- tests/example_conv.cc
+++ tests/example_conv.cc
@@ -1,55 +1,64 @@
-// example_conv.cc
-// This example demonstrates how an FK table may be convoluted with a given PDF
+// FastKernel table convolution
+// ============================
+// This example demonstrates how an **FK** table may be read from file and convoluted with a given PDF.
 
-#include <vector>
+// For this demonstration, we need some boilerplate headers
 #include <iostream>
 #include <cstdlib>
-#include <fstream>
-#include <string>
-#include <time.h>
-#include <sys/time.h>
-#include <cstdio>
 
+// Along with **LHAPDF** to provide initial scale PDFs
 #include "LHAPDF/LHAPDF.h"
 
-#include "appl_grid/appl_grid.h"
+// And the required **APFELgrid** headers
 #include "APFELgrid/fastkernel.h"
 #include "APFELgrid/transform.h"
 
+// We start with some boilerplate, a typedef named *ctype* so that we may switch between
+// double and single precision convolutions simply, and a handle for the **LHAPDF** (v5-style)
+// interface for **PDF** evolution (as per standard **APPLgrid** procedure).
 typedef float ctype;
-static double* lha_pdf;
-
 extern "C" void evolvepdf_(const double& , const double& , double* );
+
+// Now we setup an equivalent bit of boilerplate for the **FK** convolution.
+// Unlike **APPLgrids**, **FK** tables require PDFs in the DGLAP or Evolution (EVLN) basis
+// (see the reference manual for details). **APFELgrid** provides a helper utility to
+// perform the rotation to the EVLN basis from the **LHAPDF** basis. 
+// No implicit rotation is performed, as this would require prior knowledge of the
+// internal representation of PDFs used in the fit.
+
+// The function *fkpdf* therefore interfaces the **LHAPDF** call *evolvepdf_* with
+// the **FK** convolution by means of this rotation. The double *lha_pdf* holds the
+// intermediate values in the **LHAPDF** basis. Here the member argument *n* is unused.
+static double* lha_pdf;
 void fkpdf (const double& x, const double& Q, const size_t& n, ctype* pdf)
 {
   evolvepdf_(x,Q,lha_pdf);
   NNPDF::LHA2EVLN<double, ctype>(lha_pdf, pdf);
 }
 
+
+// With the boilerplate completed, we start the main loop by initialising the *lha_pdf* array
 int main(int argc, char* argv[]) {
-	// Initialise temp. array
 	lha_pdf = new double[13];
 
-	// Read FK table
+	// The **FK** table is then read from file, and a PDF set is initialised
 	std::ifstream infile; infile.open("./tests/atlas-Z0-rapidity.fk");
 	NNPDF::FKTable<ctype> FK(infile);
-
-	// Initialise PDF set
 	LHAPDF::initPDFSet("NNPDF30_nlo_as_0118", LHAPDF::LHGRID, 0);
+
+	// We now allocate an array (of type *ctype*) for results and perform the convolution.
+	// The arguments to *FK::Convolute* here are
+	// + *fkpdf* - a pointer to a function providing PDFs for individual values of *x*, *Q* and member *n* as defined above
+	// + *1* - Here we only require one PDF member to be convoluted, but this may be increased if required.
+	// + *results* - The results array
 	ctype* results = new ctype[FK.GetNData()];
+	FK.Convolute(fkpdf, 1, results);
 
-	const int nIte = 1E5;
-	timeval startTime; gettimeofday(&startTime, NULL);
-	for (int i=0; i<nIte; i++) FK.Convolute(fkpdf, 1, results);
-	timeval endTime; gettimeofday(&endTime, NULL);
-
-	for (int i=0; i < FK.GetNData(); i++)
+	// For a single member convolution, the results of the product can be simply displayed as so
+    for (int i=0; i < FK.GetNData(); i++)
 		std::cout << results[i] <<std::endl;
 
-	double seconds  = endTime.tv_sec  - startTime.tv_sec;
-    double useconds = endTime.tv_usec - startTime.tv_usec;
-    std::cout << "Timer: " << (1E6f*seconds + useconds)/(nIte*FK.GetNData()) <<" us per point"<<std::endl;
-
+	// Finally we clean up and end the program.
 	delete[] results;
 	delete[] lha_pdf;
 	exit(0);
--- tests/example_gen.cc
+++ tests/example_gen.cc
@@ -1,46 +1,77 @@
-// example_gen.cc
-// This example demonstrates how an APPLgrid can be combined
-// with evolution factors provided by APFEL to produce a FastKernel (FK)
-// table.
+// FastKernel table generation
+// ===========================
+// Here we shall demonstrate with a working example how an **APPLgrid** may be
+// combined with evolution factors provided by **APFEL** to produce a **FastKernel**
+// (**FK**) table
 
-#include <iostream>
-#include <fstream>
-#include <string>
-
+// To do this we require **APFEL**
 #include "APFEL/APFEL.h"
+// The **APFELgrid** plugin
 #include "APFELgrid/APFELgrid.h"
 #include "APFELgrid/fastkernel.h"
-
+// and **APPLgrid**
 #include "appl_grid/appl_grid.h"
 
-static const double Q0 = 1.0; // 1 GeV initial scale
+// We will also demonstrate how said **FK** table may be written to and read from file.
+// To do so we will need the following headers.
+#include <iostream>
+#include <fstream>
 
+// Beginning the main loop
 int main(int argc, char* argv[]) {
 
-  // Initialise APFEL
-  APFEL::SetTheory(string("QCD"));
-  APFEL::SetAlphaQCDRef(0.118, 91.2);
-  APFEL::SetPerturbativeOrder(1);
-
+  // We start by initialising the general evolution parameters of **APFEL**.
+  // Here we are using the exact solution of NLO DGLAP with a strong coupling
+  // of 0.118 evaluated at the Z mass. Our evolution and alpha_S running is limited
+  // to five flavours
+  APFEL::SetTheory("QCD");
   APFEL::SetPDFEvolution("exactalpha");
   APFEL::SetAlphaEvolution("exact");
 
-  // Needs to match with the APPLgrid
+  APFEL::SetAlphaQCDRef(0.118, 91.2);
+  APFEL::SetPerturbativeOrder(1);
   APFEL::SetMaxFlavourAlpha(5);
   APFEL::SetMaxFlavourPDFs(5);
 
-  // Read APPLgrid
+  // Next we read the **APPLgrid** through the usual procedure
   appl::grid g("./tests/atlas-Z0-rapidity.root");
 
-  // Set x-grid
+  // Once the **APPLgrid** is read, we can specify the initial-scale grid of x-points
+  // to be used for the interpolation of the evolution factors. To do this
+  // we make use of the **APFELgrid** helper function *get_appl_Xmin* which returns the lowest
+  // x-value required by the **APPLgrid** in question.
   APFEL::SetNumberOfGrids(2);
   APFEL::SetGridParameters(1,15,5, APFELgrid::get_appl_Xmin(g, true) );
   APFEL::SetGridParameters(2,15,5,1e-1);
 
-  // Generate FK table
-  NNPDF::FKTable<double>* FK = APFELgrid::computeFK("ATLASZRAP", g, Q0, false);
+  // With the x-grid specified, we can now generate the **FK** table. Firstly we specify
+  // and initial scale from which the evolution is to be performed. Here we use an initial
+  // scale of *Q0 = 1 GeV*.
+  const double Q0 = 1.0;
 
-  // Write FK table to file
+  // Next we must specify if the **APPLgrid** has the pdf weighting option enabled.
+  // It is not currently possible to judge this from **APFELgrid**, so it must be specified externally.
+  const bool pdfweight = false;
+
+  // The **FK** table is then generated with a single call to computeFK. Its arguments are
+  // + *"ATLASZRAP"*  - the requested name of the **FK** table
+  // + *g*            - the **APPLgrid** to be combined
+  // + *Q0*           - the requested initial scale
+  // + *pdfweight*    - the boolean specifying whether or not the grid is weighted.
+
+  // *computeFK* will then return an *NNPDF::FKTable<double>*. The template parameter specifies
+  // the precision of the internal representation of the FK table, along with the precision of its
+  // output after convolution. For generation purposes this is fixed at double precision, however
+  // the grid may be later read at single precision to enjoy the benefits of faster convolution.
+  NNPDF::FKTable<double>* FK = APFELgrid::computeFK("ATLASZRAP", g, Q0, pdfweight);
+
+  // Now the user may add various tags to the FK table as they wish, to help identify e.g
+  // the theory parameters used in its generation later. For example:
+  FK->AddTag(NNPDF::FKHeader::THEORYINFO, "TAG_KEY", "TAG_VALUE");
+  FK->AddTag(NNPDF::FKHeader::THEORYINFO, "PertubativeOrder_asString", "NLO");
+  FK->AddTag(NNPDF::FKHeader::THEORYINFO, "PertubativeOrder_asInt", 1);
+
+  // Finally we write the new FK table to file, exit and close the main loop
   std::ofstream outfile; outfile.open("./tests/atlas-Z0-rapidity.fk");
   FK->Print(outfile); outfile.close();
   
