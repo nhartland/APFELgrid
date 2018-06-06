72ca94b3e436e632e18201d7f25fec611089e4d8
Nathan Hartland <n.p.hartland@vu.nl>
2018-06-07 01:33:24 +0200

    Move docs to master branch

--- docco_generate.sh
+++ docco_generate.sh
@@ -1,1 +1,1 @@
-docco -L docco_cpp.json -l linear ./README.md ./tests/example_gen.cc ./tests/example_conv.cc
+docco -L docco_cpp.json -c docco_fixed.css -l linear ./README.md ./tests/example_gen.cc ./tests/example_conv.cc
