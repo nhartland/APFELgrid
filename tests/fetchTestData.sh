7372e896f51a9d97f28ab48f8a2777732a849b19
Valerio Bertone <valerio.bertone@cern.ch>
2018-09-07 11:20:09 +0200

    fixing fetchTestData.sh script

--- tests/fetchTestData.sh
+++ tests/fetchTestData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Fetch APPLgrids and LHgrids for testing the APFELgrid plugin
 lhapdf install CT10
-wget http://www.hepforge.org/archive/applgrid/atlas-Z0-arxiv-1109.5141.tgz
+wget http://applgrid.hepforge.org/downloads/atlas-Z0-arxiv-1109.5141.tgz
 tar -xvzf ./atlas-Z0-arxiv-1109.5141.tgz --strip-components 2
 rm atlas-Z0-arxiv-1109.5141.tgz
 mv atlas-Z0-rapidity.root ./tests
